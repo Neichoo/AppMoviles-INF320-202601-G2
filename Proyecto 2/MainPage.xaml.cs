d2340d02467b46fffbcea062b4891199b6609ecc
Dan González <dangonzacu@gmail.com>
2026-05-21 13:45:07 -0400

    Fix

--- Proyecto 2/MainPage.xaml.cs
+++ Proyecto 2/MainPage.xaml.cs
@@ -12,53 +12,5 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 		viewModel = new UserViewModel();
 		BindingContext = viewModel;
-		//ActualizarValores();
 	}
-
-	private void PHclick(object? sender, EventArgs e)
-	{
-		return;
-	}
-
-	private void PHchange(object? sender, ValueChangedEventArgs e)
-	{
-		return;
-	}
-
-	/*
-	private void OnCounterClicked(object? sender, EventArgs e)
-	{
-		ColorRed.Value = random.Next(256);
-		ColorGreen.Value = random.Next(256);
-		ColorBlue.Value = random.Next(256);
-
-		ActualizarValores();
-	}
-
-	private void AlCambiarValorSlider(object? sender, ValueChangedEventArgs e)
-	{
-		ActualizarValores();
-	}
-
-	private async void AlCopiarHexadecimalClicked(object? sender, EventArgs e)
-	{
-		await Clipboard.Default.SetTextAsync(EtiquetaHexadecimal.Text ?? "#000000");
-		await DisplayAlertAsync("Copiado", "Codigo hexadecimal copiado al portapapeles.", "OK");
-	}
-
-	private void ActualizarValores()
-	{
-		byte valorRojo = (byte)ColorRed.Value;
-		byte valorVerde = (byte)ColorGreen.Value;
-		byte valorAzul = (byte)ColorBlue.Value;
-
-		PaginaHome.BackgroundColor = Color.FromRgb(valorRojo, valorVerde, valorAzul);
-
-		EtiquetaValorRojo.Text = valorRojo.ToString();
-		EtiquetaValorVerde.Text = valorVerde.ToString();
-		EtiquetaValorAzul.Text = valorAzul.ToString();
-
-		EtiquetaHexadecimal.Text = $"#{valorRojo:X2}{valorVerde:X2}{valorAzul:X2}";
-	}
-	*/
 }
--- Proyecto 2/ViewModel.cs
+++ Proyecto 2/ViewModel.cs
@@ -5,23 +5,53 @@ using CommunityToolkit.Mvvm.Input;
 
 public partial class UserViewModel : ObservableObject
 {
-	[ObservableProperty] private string Total;
-	[ObservableProperty] private string Tip;
-	[ObservableProperty] private string N;
+	// Total cuenta y por persona
+	[ObservableProperty] private float amount;
+	[ObservableProperty] private float total;
+	// Propina porcentaje y decimal
+	[ObservableProperty] private int tipPercentage;
+	[ObservableProperty] private float tip;
+	// Cantidad de personas
+	[ObservableProperty] private int n;
 
-	public UserViewModel()
-	{
-		Total = "";
-		Tip = "";
-		N = "";
-	}
+    public UserViewModel()
+    {
+        //CalculateTotal();
+    }
+	/*
+    // Cambio de valor
+    partial void OnAmountChanged(float value) => CalculateTotal();
+	// Cambio de porcentaje slider
+    partial void OnTipPercentageChanged(int value) => CalculateTotal();
+    partial void OnNChanged(int value) => CalculateTotal();
+    
 
-	[RelayCommand]
+    // Comandos para los botones
+    [RelayCommand]
+    private void SetTip(int percentage)
+    {
+        //TipPercentage = percentage;
+    }
 
-	public void LoadUser()
-	{
-		Total = "$0";
-		Tip = "0%";
-		N = "1";
-	}
+    [RelayCommand]
+    private void ChangePeopleCount(int amount)
+    {
+        
+		
+		N += amount;
+		if(N < 1) N = 1;
+		if(N > 4) N = 4;
+		
+    }
+
+    // Lógica matemática
+    private void CalculateTotal()
+    {
+        if (N <= 0) return;
+		
+        Tip = Amount * (float)(TipPercentage / 100);
+        Total = (Amount + Tip) / N;
+		
+    }
+	*/
 }
