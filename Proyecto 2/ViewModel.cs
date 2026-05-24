7a98a44eadd534553ddf183002428c592d72eaab
smunoz3 <sebamunoz003@gmail.com>
2026-05-24 19:45:39 -0400

    correccion error int32

--- Proyecto 2/ViewModel.cs
+++ Proyecto 2/ViewModel.cs
@@ -4,7 +4,8 @@ using System.Globalization;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
-public class CalculadoraPropinaViewModel : ObservableObject{
+public class CalculadoraPropinaViewModel : ObservableObject
+{
     private string totalConsumoTexto = string.Empty;
     private int cantidadPersonas = 1;
     private double porcentajePropina;
@@ -12,71 +13,91 @@ public class CalculadoraPropinaViewModel : ObservableObject{
     private decimal propinaPorPersona;
     private decimal totalPorPersona;
 
-    public CalculadoraPropinaViewModel(){
-        SeleccionarPropinaCommand = new RelayCommand<int>(SeleccionarPropina);
-        CambiarCantidadPersonasCommand = new RelayCommand<int>(CambiarCantidadPersonas);
+    public CalculadoraPropinaViewModel()
+    {
+        SeleccionarPropinaCommand = new RelayCommand<object?>(SeleccionarPropina);
+        CambiarCantidadPersonasCommand = new RelayCommand<object?>(CambiarCantidadPersonas);
         Recalcular();
     }
 
-    public string TotalConsumoTexto{
+    public string TotalConsumoTexto
+    {
         get => totalConsumoTexto;
-        set{
-            if (SetProperty(ref totalConsumoTexto, value)){
+        set
+        {
+            if (SetProperty(ref totalConsumoTexto, value))
+            {
                 Recalcular();
             }
         }
     }
 
-    public int CantidadPersonas{
+    public int CantidadPersonas
+    {
         get => cantidadPersonas;
-        set{
+        set
+        {
             var valorNormalizado = value < 1 ? 1 : value;
-            if (SetProperty(ref cantidadPersonas, valorNormalizado)){
+            if (SetProperty(ref cantidadPersonas, valorNormalizado))
+            {
                 OnPropertyChanged(nameof(CantidadPersonasTexto));
                 Recalcular();
             }
         }
     }
 
-    public double PorcentajePropina{
+    public double PorcentajePropina
+    {
         get => porcentajePropina;
-        set{
+        set
+        {
             var valorNormalizado = Math.Round(value);
-            if (valorNormalizado < 0){
+
+            if (valorNormalizado < 0)
                 valorNormalizado = 0;
-            }
-            if (valorNormalizado > 50){
+
+            if (valorNormalizado > 50)
                 valorNormalizado = 50;
-            }
-            if (SetProperty(ref porcentajePropina, valorNormalizado)){
+
+            if (SetProperty(ref porcentajePropina, valorNormalizado))
+            {
                 OnPropertyChanged(nameof(PorcentajePropinaTexto));
                 Recalcular();
             }
         }
     }
 
-    public decimal SubtotalPorPersona{
+    public decimal SubtotalPorPersona
+    {
         get => subtotalPorPersona;
-        private set{
-            if (SetProperty(ref subtotalPorPersona, value)){
+        private set
+        {
+            if (SetProperty(ref subtotalPorPersona, value))
+            {
                 OnPropertyChanged(nameof(SubtotalPorPersonaTexto));
             }
         }
     }
 
-    public decimal PropinaPorPersona{
+    public decimal PropinaPorPersona
+    {
         get => propinaPorPersona;
-        private set{
-            if (SetProperty(ref propinaPorPersona, value)){
+        private set
+        {
+            if (SetProperty(ref propinaPorPersona, value))
+            {
                 OnPropertyChanged(nameof(PropinaPorPersonaTexto));
             }
         }
     }
 
-    public decimal TotalPorPersona{
+    public decimal TotalPorPersona
+    {
         get => totalPorPersona;
-        private set{
-            if (SetProperty(ref totalPorPersona, value)){
+        private set
+        {
+            if (SetProperty(ref totalPorPersona, value))
+            {
                 OnPropertyChanged(nameof(TotalPorPersonaTexto));
             }
         }
@@ -87,18 +108,50 @@ public class CalculadoraPropinaViewModel : ObservableObject{
     public string SubtotalPorPersonaTexto => SubtotalPorPersona.ToString("N2", CultureInfo.CurrentCulture);
     public string PropinaPorPersonaTexto => PropinaPorPersona.ToString("N2", CultureInfo.CurrentCulture);
     public string TotalPorPersonaTexto => TotalPorPersona.ToString("N2", CultureInfo.CurrentCulture);
-    public IRelayCommand<int> SeleccionarPropinaCommand { get; }
-    public IRelayCommand<int> CambiarCantidadPersonasCommand { get; }
 
-    private void SeleccionarPropina(int porcentaje){
-        PorcentajePropina = porcentaje;
+    public IRelayCommand<object?> SeleccionarPropinaCommand { get; }
+    public IRelayCommand<object?> CambiarCantidadPersonasCommand { get; }
+
+    private void SeleccionarPropina(object? parameter)
+    {
+        if (TryGetInt(parameter, out int porcentaje))
+        {
+            PorcentajePropina = porcentaje;
+        }
     }
 
-    private void CambiarCantidadPersonas(int cambio){
-        CantidadPersonas += cambio;
+    private void CambiarCantidadPersonas(object? parameter)
+    {
+        if (TryGetInt(parameter, out int cambio))
+        {
+            CantidadPersonas += cambio;
+        }
     }
 
-    private void Recalcular(){
+    private static bool TryGetInt(object? parameter, out int value)
+    {
+        if (parameter is int entero)
+        {
+            value = entero;
+            return true;
+        }
+
+        if (parameter is string texto && int.TryParse(texto, NumberStyles.Integer, CultureInfo.CurrentCulture, out value))
+        {
+            return true;
+        }
+
+        if (parameter is string textoInvariant && int.TryParse(textoInvariant, NumberStyles.Integer, CultureInfo.InvariantCulture, out value))
+        {
+            return true;
+        }
+
+        value = 0;
+        return false;
+    }
+
+    private void Recalcular()
+    {
         var totalConsumo = ConvertirADecimal(TotalConsumoTexto);
         var factorPropina = (decimal)PorcentajePropina / 100m;
 
@@ -107,16 +160,17 @@ public class CalculadoraPropinaViewModel : ObservableObject{
         TotalPorPersona = decimal.Round(SubtotalPorPersona + PropinaPorPersona, 2);
     }
 
-    private static decimal ConvertirADecimal(string texto){
-        if (string.IsNullOrWhiteSpace(texto)){
+    private static decimal ConvertirADecimal(string texto)
+    {
+        if (string.IsNullOrWhiteSpace(texto))
             return 0m;
-        }
-        if (decimal.TryParse(texto, NumberStyles.Number, CultureInfo.CurrentCulture, out var valorLocal)){
+
+        if (decimal.TryParse(texto, NumberStyles.Number, CultureInfo.CurrentCulture, out var valorLocal))
             return valorLocal;
-        }
-        if (decimal.TryParse(texto, NumberStyles.Number, CultureInfo.InvariantCulture, out var valorInvariante)){
+
+        if (decimal.TryParse(texto, NumberStyles.Number, CultureInfo.InvariantCulture, out var valorInvariante))
             return valorInvariante;
-        }
+
         return 0m;
     }
 }
